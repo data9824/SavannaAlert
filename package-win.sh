1eacc327d4dfee6d7c42702e4884df3b673aae4d
Takuya Nishida <data9824@server.fake>
2016-04-02 20:50:49 +0900

    バージョン番号の更新。

---
+++ package-win.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf pack/SavannaAlert-win32-ia32/
+./node_modules/.bin/electron-packager . SavannaAlert --platform=win32 --arch=ia32 --version=0.36.12 --ignore pack --ignore src --ignore typings --ignore build --ignore .idea --ignore doc --out pack --overwrite
+cp package.json pack/SavannaAlert-win32-ia32/resources/app/
+cp -r node_modules/node-notifier/ pack/SavannaAlert-win32-ia32/resources/app/node_modules/
+# 壊れたZIPファイルが生成されてしまう
+# pushd .
+# cd pack/
+# rm SavannaAlert-win32-ia32.zip
+# zip -r SavannaAlert-win32-ia32.zip SavannaAlert-win32-ia32
+# popd
