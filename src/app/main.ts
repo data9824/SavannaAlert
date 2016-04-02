1175e168b7f24823d0774d04dd114b0e2ec5cd6a
Takuya Nishida <data9824@server.fake>
2016-04-02 15:57:19 +0900

    最初のコミット。

---
+++ src/app/main.ts
@@ -0,0 +1,177 @@
+/// <reference path="../../typings/browser.d.ts" />
+import BrowserWindow = Electron.BrowserWindow;
+
+import * as electron from 'electron';
+import IPCMain = Electron.IPCMain;
+import IPCMainEvent = Electron.IPCMainEvent;
+import {Socket} from "net";
+import {Stats} from "fs";
+import {parse} from "querystring";
+import {IncomingMessage} from "http";
+import {NodeNotifier} from "node-notifier";
+let fs: any = require('fs');
+let http: any = require('http');
+let notifier: NodeNotifier = require('node-notifier');
+let app: Electron.App = electron.app;
+let dialog: Electron.Dialog = electron.dialog;
+let mainWindow: BrowserWindow = undefined;
+
+function createWindow() {
+	'use strict';
+	if (mainWindow !== undefined) {
+		mainWindow.focus();
+		return;
+	}
+	let browserWindowOptions: Electron.BrowserWindowOptions = {
+		width: 800,
+		height: 600,
+		webPreferences: {
+			plugins: true,
+		},
+	};
+	mainWindow = new electron.BrowserWindow(browserWindowOptions);
+	mainWindow.setMenu(null);
+	mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
+//	mainWindow.webContents.openDevTools();
+	mainWindow.on('closed', () => {
+		mainWindow = undefined;
+	});
+}
+
+app.on('ready', () => {
+	let tray: Electron.Tray = new electron.Tray(__dirname + "/../browser/tray.png");
+	let contextMenu: Electron.Menu = electron.Menu.buildFromTemplate([
+		{
+			label: "設定...",
+			click: () => {
+				createWindow();
+			},
+		},
+		{
+			label: "バージョン情報...",
+			click: () => {
+				dialog.showErrorBox("バージョン情報", "バージョン 20160402");
+			},
+		},
+		{
+			label: "終了",
+			click: () => {
+				app.quit();
+			},
+		},
+	]);
+	tray.setToolTip("SavannaAlert");
+	tray.setContextMenu(contextMenu);
+	tray.on("click", createWindow);
+});
+
+app.on("window-all-closed", () => {
+	// do nothing not to quit the app
+});
+
+interface IChannel {
+	url: string;
+	title: string;
+}
+
+function checkBroadcasting(body: string): boolean {
+	let notBroadcastingText: string = '<div id="broadTitle"></div>';
+	let divIndex: number = body.indexOf('<div id="broadTitle">');
+	if (divIndex < 0) {
+		return false;
+	}
+	return (body.substr(divIndex, notBroadcastingText.length) !== notBroadcastingText);
+}
+
+function extractChannelTitle(body: string): string {
+	let titleMarker: string = '<span id="channelName_view">';
+	let titleIndex: number = body.indexOf(titleMarker);
+	if (titleIndex < 0) {
+		return "(タイトル不明)";
+	}
+	let end: number = body.indexOf('</span>', titleIndex);
+	if (end < 0) {
+		return "(タイトル不明(2))";
+	}
+	return body.substr(titleIndex + titleMarker.length, end - (titleIndex + titleMarker.length));
+}
+
+let channels: IChannel[] = [];
+let lastChannelBroadcastings: boolean[] = [];
+
+setInterval(() => {
+	alertChannels();
+}, 30000);
+
+function alertChannels(): void {
+	channels.forEach((channel: IChannel) => {
+		http.get(channel.url, (res: IncomingMessage) => {
+			let url: string = channel.url;
+			let body: string = "";
+			res.setEncoding("utf8");
+			res.on("data", (chunk: string) => {
+				body += chunk;
+			});
+			res.on("end", () => {
+				let title: string = extractChannelTitle(body);
+				let broadcasting: boolean = checkBroadcasting(body);
+				if (broadcasting) {
+					if (lastChannelBroadcastings[url] === undefined ||
+						lastChannelBroadcastings[url] === false) {
+						lastChannelBroadcastings[url] = true;
+						notifier.notify({
+							title: title,
+							message: url,
+							icon: __dirname + "/../browser/tray.png",
+							sound: true,
+							wait: true,
+						});
+						notifier.on("click", (notifierObject, options) => {
+							electron.shell.openExternal(options.message);
+						});
+					}
+				} else {
+					lastChannelBroadcastings[url] = false;
+				}
+			});
+		});
+	});
+}
+
+interface IConfig {
+	version: number;
+	channels: IChannel[];
+}
+
+function getConfigFileName(): string {
+	return app.getPath('userData') + "/config.json";
+}
+
+let ipcMain: IPCMain = require('electron').ipcMain;
+ipcMain.on("save", (event: IPCMainEvent, arg: string) => {
+	channels = JSON.parse(arg);
+	console.log(app.getPath('userData'));
+	let config: IConfig = {
+		version: 1,
+		channels: channels,
+	};
+	fs.writeFileSync(getConfigFileName(), JSON.stringify(config));
+});
+ipcMain.on("getChannels", (event: IPCMainEvent) => {
+	event.sender.send("getChannels", JSON.stringify(channels));
+});
+
+try {
+	let configText: string = fs.readFileSync(getConfigFileName(), "utf8");
+	let config: IConfig = JSON.parse(configText);
+	channels = config.channels;
+} catch (e) {
+	channels = [
+		{
+			url: "http://afreecatv.jp/35841790",
+			title: "",
+		},
+	];
+}
+
+alertChannels();
---
+++ src/browser/js/main.ts
@@ -0,0 +1,161 @@
+/// <reference path="../../../typings/browser.d.ts" />
+/// <reference path="../../../node_modules/vue-class-component/index.d.ts" />
+
+import * as Vue from 'vue';
+import * as VueRouter from 'vue-router';
+import VueComponent from 'vue-class-component';
+import * as $ from 'jquery';
+import * as electron from 'electron';
+let ipcRenderer: Electron.IpcRenderer = require('electron').ipcRenderer;
+
+interface IChannel {
+	url: string;
+	title: string;
+}
+
+interface IAppModelListener {
+	onAppModelChanged: (sender: AppModel) => void;
+}
+
+class AppModel {
+	private listeners: IAppModelListener[] = [];
+	public addListener(listener: IAppModelListener): void {
+		this.listeners.push(listener);
+	}
+	public removeListener(listener: IAppModelListener): void {
+		let index: number = this.listeners.indexOf(listener);
+		if (index >= 0) {
+			this.listeners.splice(index, 1);
+		}
+	}
+	public channels: IChannel[] = [];
+	public fireChanged(): void {
+		for (let i: number = 0; i < this.listeners.length; ++i) {
+			this.listeners[i].onAppModelChanged(this);
+		}
+	}
+}
+
+let appModel: AppModel = new AppModel();
+
+@VueComponent({
+	template: `
+		<div class="channel">
+			<a v-on:click.prevent="openPage" href="#">{{ item.url }}</a>
+			{{ item.title }}
+			<input class="delete" type="button" value="削除" v-on:click="delete(item)"/>
+		</div>
+	`,
+	props: ["item", "index"],
+})
+class ChannelView extends Vue {
+	public delete(item: IChannel): void
+	{
+		appModel.channels.splice(this.$get("index"), 1);
+		ipcRenderer.send("save", JSON.stringify(appModel.channels));
+		appModel.fireChanged();
+	}
+	public openPage(): void {
+		electron.shell.openExternal(this.$get("item").url);
+	}
+}
+
+@VueComponent({
+	template: `
+		<div class="channelList" v-for="channel in channels">
+			<channel :item="channel" :index="$index"></channel>
+		</div>
+	`,
+	props: ['channels'],
+})
+class ChannelListView extends Vue {
+}
+
+@VueComponent({
+	template: `
+		<form class="channelForm" v-on:submit.prevent="onSubmit">
+			<input class="url" type="text" placeholder="チャンネルのURL" v-model="url"/>
+			<input type="submit" value="追加"/>
+		</form>
+	`,
+})
+class ChannelFormView extends Vue {
+	private url: string;
+	public data(): any {
+		return {
+			url: '',
+		};
+	}
+	public onSubmit(): void {
+		if (this.url === '') {
+			return;
+		}
+		this.$dispatch('add-channel', {
+			url: this.url,
+		});
+		this.url = '';
+	}
+}
+
+interface IAddingChannel {
+	url: string;
+}
+
+@VueComponent({
+	template: `
+		<div class="channelBox">
+			<h1>チャンネル一覧</h1>
+			<channel-list :channels="channels"></channel-list>
+			<channel-form v-on:add-channel="addChannel"></channel-form>
+		</div>
+	`,
+})
+class ChannelBoxView extends Vue implements IAppModelListener {
+	private channels: IChannel[];
+	public data(): any {
+		return {
+			channels: appModel.channels,
+		};
+	}
+	public attached(): void {
+		ipcRenderer.send("getChannels");
+		ipcRenderer.on("getChannels", (e: any, arg: string) => {
+			appModel.channels = JSON.parse(arg);
+			appModel.fireChanged();
+		});
+		appModel.addListener(this);
+	}
+	public detached(): void {
+		appModel.removeListener(this);
+	}
+	public addChannel(item: IAddingChannel): void {
+		appModel.channels.push({
+			url: item.url,
+			title: "",
+		});
+		ipcRenderer.send("save", JSON.stringify(appModel.channels));
+		appModel.fireChanged();
+	}
+	public onAppModelChanged(sender: AppModel): void {
+		this.channels = appModel.channels;
+	}
+}
+
+@VueComponent({})
+class App extends Vue {
+}
+
+Vue.component('channel', ChannelView);
+Vue.component('channel-list', ChannelListView);
+Vue.component('channel-form', ChannelFormView);
+Vue.use(VueRouter);
+let router: any = new VueRouter();
+router.map({
+	'/': {
+		component: ChannelBoxView,
+	},
+});
+
+$(function() {
+	router.start(App, '#content');
+});
