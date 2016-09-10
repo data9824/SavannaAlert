43e8e3c0a4f65db5cba65186fedfd7d2d4374b74
Takuya Nishida <data9824@server.fake>
2016-09-10 20:29:01 +0900

    パッケージング時のエラーを修正。

--- package-mac.sh
+++ package-mac.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-rm -rf pack/SavannaAlert-darwin-x64/
+rm -rf release/SavannaAlert-darwin-x64/
 ./node_modules/.bin/electron-packager . SavannaAlert --platform=darwin --arch=x64 --version=1.3.5 \
-    --ignore=pack --ignore=src --ignore=typings --ignore=build --ignore=.idea --ignore=resources \
-    --ignore=node_modules/del \
-    --ignore=node_modules/gulp \
-    --ignore=node_modules/gulp-sass \
-    --ignore=node_modules/gulp-typescript \
-    --ignore=node_modules/gulp-webpack \
-    --ignore=node_modules/webpack \
+    --ignore=release --ignore=src --ignore=typings --ignore=build --ignore=.idea --ignore=resources \
     --icon=resources/app.icns \
-    --out pack --overwrite
-cp package.json pack/SavannaAlert-darwin-x64/SavannaAlert.app/Contents/Resources/app/
-cp -r node_modules/node-notifier/ pack/SavannaAlert-darwin-x64/SavannaAlert.app/Contents/Resources/app/node_modules/node-notifier
+    --out release --overwrite
+cp -r node_modules/node-notifier/ release/SavannaAlert-darwin-x64/SavannaAlert.app/Contents/Resources/app/node_modules/node-notifier
 pushd .
-cd pack/SavannaAlert-darwin-x64/
+cd release/SavannaAlert-darwin-x64/
 rm ../SavannaAlert-darwin-x64.zip
 zip -r ../SavannaAlert-darwin-x64.zip SavannaAlert.app
 popd
--- package-win.sh
+++ package-win.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-rm -rf pack/SavannaAlert-win32-ia32/
+rm -rf release/SavannaAlert-win32-ia32/
 ./node_modules/.bin/electron-packager . SavannaAlert --platform=win32 --arch=ia32 --version=1.3.5 \
-    --ignore=pack --ignore=src --ignore=typings --ignore=build --ignore=.idea --ignore=resources \
-    --ignore=node_modules/del \
-    --ignore=node_modules/gulp \
-    --ignore=node_modules/gulp-sass \
-    --ignore=node_modules/gulp-typescript \
-    --ignore=node_modules/gulp-webpack \
-    --ignore=node_modules/webpack \
+    --ignore=release --ignore=src --ignore=typings --ignore=build --ignore=.idea --ignore=resources \
     --icon=resources/app.ico \
     --version-string.FileDescription=SavannaAlert \
     --version-string.ProductName=SavannaAlert \
     --version-string.OriginalFilename=SavannaAlert.exe \
     --version-string.LegalCopyright="CC0 1.0 Universal" \
-    --out pack --overwrite
-cp package.json pack/SavannaAlert-win32-ia32/resources/app/
+    --out release --overwrite
 cp -r node_modules/node-notifier/ pack/SavannaAlert-win32-ia32/resources/app/node_modules/
 # 壊れたZIPファイルが生成されてしまう
 # pushd .
-# cd pack/
+# cd release/
 # rm SavannaAlert-win32-ia32.zip
 # zip -r SavannaAlert-win32-ia32.zip SavannaAlert-win32-ia32
 # popd
